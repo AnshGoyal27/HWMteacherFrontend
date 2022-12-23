fbc5bcdda517d5698d3fe9bd6783de79b1a48599
Ansh Goyal <110151117+AnshGoyal27@users.noreply.github.com>
2022-12-23 11:24:33 +0530

    Update mainpage.jsx

--- src/source/pages/mainpage.jsx
+++ src/source/pages/mainpage.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom"
 import { QR } from "../components/qrgenerator"
 import { ClassList } from "./classlist"
 import { Login } from "./login"
-import { WorkOption } from "./option"
 
 export const MainPage=()=>{
     return(
