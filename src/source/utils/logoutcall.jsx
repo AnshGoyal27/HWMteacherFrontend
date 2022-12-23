57d8170800ab349291b75ef4e7ab3fa046476a90
Ansh Goyal <110151117+AnshGoyal27@users.noreply.github.com>
2022-12-23 11:25:33 +0530

    Update logoutcall.jsx

--- src/source/utils/logoutcall.jsx
+++ src/source/utils/logoutcall.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { ActionCreator, newAction } from "./redux/action";
+import { ActionCreator } from "./redux/action";
 import { store } from "./redux/store";
 
 export default function logoutcall() {
