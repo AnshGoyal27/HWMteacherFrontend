f4fa62634840d706cacde5646645f328d02ce47c
Ansh Goyal <110151117+AnshGoyal27@users.noreply.github.com>
2023-03-26 10:45:26 +0530

    Update login.js

--- src/source/utils/axios/login.js
+++ src/source/utils/axios/login.js
@@ -7,12 +7,14 @@ export function LoginCall(userName,ID){
         method:'post',
         mode: 'cors',
         url:'https://hw-mteacher-backend-5oth7ar2h-anshgoyal27.vercel.app/login',
-        headers:{
-            'Access-Control-Allow-Origin':"*",
-            'Access-Control-Allow-Methods': "OPTIONS, POST, GET",
-            'Access-Control-Allow-Credentials': "true",
-            'Access-Control-Allow-Headers': "Content-Type"
-        },
+        headers: {
+                "Access-Control-Allow-Origin": "https://musicstorebm.netlify.app",
+                "Access-Control-Allow-Credentials": "true",
+                "Access-Control-Allow-Methods": 'HEAD, GET, POST, PUT, DELETE, OPTIONS',
+                "Access-Control-Allow-Headers" : 'Origin,Content-Type,Authorization',
+                'Accept': "application/json",
+                "Content-Type": "application/json;charset=UTF-8"
+            },
         data:{
             userName:userName,
             password:ID,
